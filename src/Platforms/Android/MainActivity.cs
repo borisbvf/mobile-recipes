c4fb6185a9bd843cf676cd772b665c8aa45c25bc
borisbvf <borisbvf@outlook.com>
2025-04-21 20:25:28 +0900

    Make unified color scheme.

--- src/Platforms/Android/MainActivity.cs
+++ src/Platforms/Android/MainActivity.cs
@@ -1,11 +1,66 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Android.Content.Res;
+using Microsoft.Maui.Controls.Compatibility.Platform.Android;
+using Android.Views;
+using System.ComponentModel;
 
 namespace Recipes
 {
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
-    }
+		protected override void OnCreate(Bundle? savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+			RefreshStatusBarColor();
+			ThemeService.Instance.PropertyChanged += OnSettingsPropertyChanged;
+		}
+
+		public override void OnConfigurationChanged(Configuration newConfig)
+		{
+			base.OnConfigurationChanged(newConfig);
+			if (Build.VERSION.SdkInt < BuildVersionCodes.R)
+				return;
+			if (ThemeService.Instance?.Theme != AppTheme.Unspecified)
+				return;
+			if (newConfig.IsNightModeActive)
+			{
+				Window?.SetStatusBarColor(GetColorFromRes("ShellBackgroundDark"));
+				Window?.InsetsController?.SetSystemBarsAppearance(0, (int)WindowInsetsControllerAppearance.LightStatusBars);
+			}
+			else
+			{
+				Window?.SetStatusBarColor(GetColorFromRes("ShellBackgroundLight"));
+				Window?.InsetsController?.SetSystemBarsAppearance((int)WindowInsetsControllerAppearance.LightStatusBars, (int)WindowInsetsControllerAppearance.LightStatusBars);
+			}
+		}
+
+		private void OnSettingsPropertyChanged(object? sender, PropertyChangedEventArgs e)
+		{
+			if (e.PropertyName == nameof(ThemeService.Theme))
+			{
+				RefreshStatusBarColor();
+			}
+		}
+
+		private void RefreshStatusBarColor()
+		{
+			if (Build.VERSION.SdkInt < BuildVersionCodes.R)
+				return;
+
+			AppTheme? currentTheme = ThemeService.Instance?.Theme;
+			if (currentTheme == AppTheme.Light || currentTheme == AppTheme.Unspecified && !Resources.Configuration.IsNightModeActive)
+				Window?.SetStatusBarColor(GetColorFromRes("MainLight"));
+			else if (currentTheme == AppTheme.Dark || currentTheme == AppTheme.Unspecified && Resources.Configuration.IsNightModeActive)
+				Window?.SetStatusBarColor(GetColorFromRes("MainDark"));
+		}
+
+		private Android.Graphics.Color GetColorFromRes(string name)
+		{
+			// Get value from Resources/Styles/Colors.xaml
+			return ((Color)App.Current!.Resources[name]).ToAndroid();
+		}
+	}
 }
